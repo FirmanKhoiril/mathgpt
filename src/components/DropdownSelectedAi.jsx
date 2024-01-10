d389b0521ba2fa36b3dd1d2c9b1757cdc74b508c
FirmanKhoiril <firmankhoiril13@gmail.com>
2024-01-10 20:01:20 +0700

    test

--- src/components/DropdownSelectedAi.jsx
+++ src/components/DropdownSelectedAi.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelectedAi } from "../hooks/useSelectedAi";
+import {useLocation} from 'react-router-dom'
 
 const DropdownSelectedAi = () => {
-  const {setSelectedAi, selectedAi} = useSelectedAi()
-  const selectedAiSolve = selectedAi.get("selected");
+  const { setSelectedAi, selectedAi } = useSelectedAi();
+  const [selectedAiValue, setSelectedAiValue] = useState(selectedAi.get("selected"));
 
   const aiSelectedOptions = [
     {
@@ -12,23 +13,43 @@ const DropdownSelectedAi = () => {
     },
     {
       value: 'ai-explainer',
-      name: 'Ai Explainerr'
+      name: 'Ai Explainer'
     },
     {
       value: 'ai-writer',
-      name: 'Ai Writerr'
+      name: 'Ai Writer'
     },
-  ]
+  ];
+
+  useEffect(() => {
+    // Update the global state when the local state changes
+    setSelectedAi((prev) => {
+      prev.set("selected", selectedAiValue);
+      return prev
+    });
+    
+    // Save the selectedAiValue to localStorage
+  }, [ selectedAi]);
+
+  const handleSelectChange = (e) => {
+    const selectedValue = e.currentTarget.value;
+    setSelectedAi((prev) => {
+      prev.set("selected", selectedValue);
+      return prev
+    });
+    setSelectedAiValue(selectedValue);
+  };
 
   return (
-    <select onChange={(e) => {
-      setSelectedAi((prev) => {
-        prev.set("selected", e.currentTarget.value )
-        return prev
-      })
-    }} className="dropdown__selected__ai__container">
+    <select
+      onChange={handleSelectChange}
+      value={selectedAiValue}
+      className="dropdown__selected__ai__container"
+    >
       {aiSelectedOptions.map((item) => (
-        <option value={item.value}>{selectedAiSolve === item.value ? item.name : item.name}</option>
+        <option key={item.value} value={item.value}>
+          {selectedAiValue === item.value ? item.name : item.name}
+        </option>
       ))}
     </select>
   );
